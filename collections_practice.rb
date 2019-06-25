45ca3c780e1518356ac4503cbd63330b7b995870
Samantha Kane <s.kane133@gmail.com>
2019-06-25 02:30:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,9 +43,9 @@ end
 
 def add_s(array)
   array[0..3].each_with_index.collect do |element, index|
-  if array[index] = 0 || array[index] > 1
+  if array[index] == 0 || array[index] > 1
     element.concat("s")
-  array
 end
 end
+array
 end
